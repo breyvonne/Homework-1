5577b76a060f717f9a6444c47aaa3e9cd0edb214
Breanna <123026437+breyvonne@users.noreply.github.com>
2023-02-05 22:09:21 -0500

    Add files via upload

---
+++ Homework1.py
@@ -0,0 +1,46 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#Breanna Eafon
+#problem 1
+type("Rosewater")
+<class 'str'>
+type(0.001)
+<class 'float'>
+type(37)
+<class 'int'>
+>>> type(False)
+<class 'bool'>
+>>> #problem 2
+>>> print ("hello", "world")
+hello world
+>>> #using the , creates a space between the two strings
+>>> print ("hello"+"world")
+helloworld
+>>> #using the + creates the strings without a space between them
+>>> 
+>>> #Problem 3
+>>> float(2)
+2.0
+>>> int(3.14)
+3
+>>> str(2)
+'2'
+>>> 
+>>> #problem 4
+>>> print("Joe Waited for the train. \nThe train was late.\nMary and Samantha took the bus.\nI looked for Mary and Samantha at the bus station.\nMary and Samantha arrived at the bus station early but waited until noon for the bus.")
+Joe Waited for the train. 
+The train was late.
+Mary and Samantha took the bus.
+I looked for Mary and Samantha at the bus station.
+Mary and Samantha arrived at the bus station early but waited until noon for the bus.
+>>> 
+>>> #problem 5
+>>> 
+>>> flour=2
+>>> milk=1
+>>> egg=4
+>>> oil=.4
+>>> vanilla=.012
+>>> cake= 2*flour+0.5*(milk-egg+vanilla)+oil**2
+>>> print(cake)
+2.6660000000000004
